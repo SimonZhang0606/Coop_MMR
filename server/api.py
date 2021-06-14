798211180aa9309f6ed482ad050d059155826548
Justin Xu <xu.justin.j@gmail.com>
2021-06-13 20:33:26 -0400

    Return jobs as dictionaries

--- server/api.py
+++ server/api.py
@@ -10,27 +10,39 @@ def list_jobs(connection):
     cursor = connection.cursor()
 
     cursor.execute("""
-    SELECT COMPANY.name as company_name, JOB.title as job_title,
-    IFNULL(avg_salary, "missing") as avg_salary, IFNULL(avg_rating, "missing") as avg_rating
+    SELECT
+        JOB.jid as jid,
+        COMPANY.name as company_name,
+        JOB.title as job_title,
+        IFNULL(avg_salary, "missing") as avg_salary,
+        IFNULL(avg_rating, "missing") as avg_rating
     FROM JOB
     LEFT OUTER JOIN COMPANY
-    ON JOB.cid = COMPANY.cid
+        ON JOB.cid = COMPANY.cid
     LEFT OUTER JOIN (SELECT jid, AVG(salary) as avg_salary
                         FROM PLACEMENT
                         WHERE salary IS NOT NULL
                         GROUP BY jid
                     ) as JOB_SALARY
-    ON JOB.jid = JOB_SALARY.jid
+        ON JOB.jid = JOB_SALARY.jid
     LEFT OUTER JOIN (SELECT jid, AVG(rating) as avg_rating
                         FROM REVIEW
                         WHERE rating IS NOT NULL
                         GROUP BY jid
                     ) as JOB_RATING
-    ON JOB.jid = JOB_RATING.jid
+        ON JOB.jid = JOB_RATING.jid
     ORDER BY JOB_SALARY.avg_salary DESC;
     """)
 
-    return cursor.fetchall()
+    for job in cursor.fetchall():
+        jid, company_name, job_title, avg_salary, avg_rating = job
+        yield {
+            "jid": int(jid),
+            "company_name": str(company_name),
+            "job_title": str(job_title),
+            "avg_salary": float(avg_salary),
+            "avg_rating": float(avg_rating),
+        }
 
 
 if __name__ == '__main__':
@@ -56,7 +68,7 @@ if __name__ == '__main__':
 
     @app.route('/jobs', methods=['GET'])
     def home():
-        jobs_list = list_jobs(connection)
+        jobs_list = list(list_jobs(connection))
         pprint(jobs_list)
         return json.dumps(jobs_list)
 
