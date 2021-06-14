08e704833ec115c0f6b2496a9ca3e265b4f55a8c
Justin Xu <xu.justin.j@gmail.com>
2021-06-13 20:23:46 -0400

    Add keys to table

--- web/src/Jobs.js
+++ web/src/Jobs.js
@@ -1,51 +1,51 @@
-import React from 'react';
+import React from "react";
 
 export default class Jobs extends React.Component {
   state = {
-    jobs: []
-  }
+    jobs: [],
+  };
 
   // Fetch jobs
   componentDidMount() {
     fetch("http://localhost:5000/jobs")
-      .then(response => response.json())
-      .then(result => {
-        this.setState({jobs: result})
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({ jobs: result });
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
   }
 
   tableRows() {
-    return this.state.jobs.map(j => (
+    return this.state.jobs.map((j) => (
       <tr>
         <td>{j.job_title}</td>
         <td>{j.company_name}</td>
         <td>{j.avg_salary}</td>
         <td>{j.avg_rating}</td>
       </tr>
-    ))
+    ));
   }
 
   render() {
     return (
       <>
-      <h1>Jobs</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Company</th>
-            <th>Avg. Salary</th>
-            <th>Avg. Rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.tableRows()}
-          <tr></tr>
-        </tbody>
-      </table>
+        <h1>Jobs</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Company</th>
+              <th>Avg. Salary</th>
+              <th>Avg. Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.tableRows()}
+            <tr></tr>
+          </tbody>
+        </table>
       </>
     );
   }
