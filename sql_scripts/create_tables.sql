9bef9c377288ae5c5e7ddcc51ce330cb2cde6684
Justin Xu <xu.justin.j@gmail.com>
2021-06-14 07:51:53 -0400

    Clean up queries

--- sql_scripts/create_tables.sql
+++ sql_scripts/create_tables.sql
@@ -1,79 +1,82 @@
--- STUDENT(SID, NAME, ENROLMENT_DATE, GRAD_DATE, PROGRAM)
--- COMPANY(CID, NAME)
--- JOB(CID, JID, MMR, TITLE, TID)
--- PLACEMENT(CID, JID, PID, SID, TERM_NUM, SALARY, START_DATE, END_DATE)
--- REVIEW(CID, JID, RID, HEADLINE, review_desc, RATING)
--- TAG(TID, LABEL)
--- JOB_TAG(CID, TID)
+-- Schema
+-- STUDENT(sid, name, program, enrol_date, grad_date)
+-- COMPANY(cid, name)
+-- JOB(cid, jid, mmr, title)
+-- PLACEMENT(cid, jid, pid, sid, term_num, salary, start_date, end_date)
+-- REVIEW(cid, jid, rid, headline, review_desc, RATING)
+-- TAG(tid, label)
+-- JOB_TAG(cid, tid)
 
+-- make sure we start from a fresh database
+DROP TABLE IF EXISTS JOB_TAG;
+DROP TABLE IF EXISTS TAG;
 DROP TABLE IF EXISTS REVIEW;
 DROP TABLE IF EXISTS PLACEMENT;
-DROP TABLE IF EXISTS JOB_TAG;
 DROP TABLE IF EXISTS JOB;
 DROP TABLE IF EXISTS COMPANY;
 DROP TABLE IF EXISTS STUDENT;
-DROP TABLE IF EXISTS TAG;
 
-CREATE TABLE TAG
-  (
-    tid         DECIMAL(9, 0) NOT NULL, 
-    label       VARCHAR(100) UNIQUE NOT NULL,
-    PRIMARY KEY (tid)
-  );
 
 CREATE TABLE STUDENT
-  ( 
-    sid         DECIMAL(9, 0) NOT NULL PRIMARY KEY, 
-    name        VARCHAR(30), 
-    program     TEXT NOT NULL, 
-    enrol_date  DATE NOT NULL, 
+  (
+    sid         DECIMAL(9, 0) NOT NULL PRIMARY KEY,
+    name        VARCHAR(30),
+    program     TEXT NOT NULL,
+    enrol_date  DATE NOT NULL,
     grad_date   DATE
-  ); 
+  );
 
-CREATE TABLE COMPANY 
-  ( 
-    cid         DECIMAL(9, 0) NOT NULL, 
+CREATE TABLE COMPANY
+  (
+    cid         DECIMAL(9, 0) NOT NULL,
     name        VARCHAR(30) NOT NULL,
     PRIMARY KEY (cid)
-  ); 
+  );
 
-CREATE TABLE JOB 
-  ( 
-    cid         DECIMAL(9, 0) NOT NULL, 
-    jid         DECIMAL(9, 0) NOT NULL, 
-    mmr         INT NOT NULL, 
+CREATE TABLE JOB
+  (
+    cid         DECIMAL(9, 0) NOT NULL,
+    jid         DECIMAL(9, 0) NOT NULL,
+    mmr         INT NOT NULL,
     title       TEXT NOT NULL,
     PRIMARY KEY (jid),
     FOREIGN KEY (cid) REFERENCES COMPANY(cid)
-  ); 
+  );
 
-CREATE TABLE PLACEMENT 
-  ( 
-     cid        DECIMAL(9, 0) NOT NULL, 
-     jid        DECIMAL(9, 0) NOT NULL, 
+CREATE TABLE PLACEMENT
+  (
+     cid        DECIMAL(9, 0) NOT NULL,
+     jid        DECIMAL(9, 0) NOT NULL,
      pid        DECIMAL(9, 0) NOT NULL,
-     sid        DECIMAL(9, 0) NOT NULL, 
+     sid        DECIMAL(9, 0) NOT NULL,
      term_num   INT NOT NULL,
      salary     DECIMAL(9, 2),
      start_date DATE NOT NULL,
      end_date   DATE,
-     PRIMARY KEY(pid), 
-     FOREIGN KEY(sid) REFERENCES STUDENT(sid), 
-     FOREIGN KEY(cid) REFERENCES COMPANY(cid), 
+     PRIMARY KEY(pid),
+     FOREIGN KEY(sid) REFERENCES STUDENT(sid),
+     FOREIGN KEY(cid) REFERENCES COMPANY(cid),
      FOREIGN KEY(jid) REFERENCES JOB(jid)
-  ); 
+  );
 
 CREATE TABLE REVIEW
   (
-    cid         DECIMAL(9, 0) NOT NULL, 
-    jid         DECIMAL(9, 0) NOT NULL, 
-    rid         DECIMAL(9, 0) NOT NULL, 
+    cid         DECIMAL(9, 0) NOT NULL,
+    jid         DECIMAL(9, 0) NOT NULL,
+    rid         DECIMAL(9, 0) NOT NULL,
     headline    TEXT NOT NULL,
     review_body TEXT NOT NULL,
     rating      INT,
     PRIMARY KEY(rid),
-    FOREIGN KEY(cid) REFERENCES COMPANY(cid), 
+    FOREIGN KEY(cid) REFERENCES COMPANY(cid),
     FOREIGN KEY(jid) REFERENCES JOB(jid)
+  );
+
+CREATE TABLE TAG
+  (
+    tid         DECIMAL(9, 0) NOT NULL,
+    label       VARCHAR(100) UNIQUE NOT NULL,
+    PRIMARY KEY (tid)
   );
 
 CREATE TABLE JOB_TAG
--- sql_scripts/test-sample.sql
+++ sql_scripts/test-sample.sql
@@ -1,9 +1,10 @@
-SELECT '--------Query 1--------' AS '';
-SELECT 'BROWSE ALL COMPANIES PAGE' AS '';
-SELECT '-----------------------' AS '';
+SELECT 'Query 1: Browse Companies page' AS '';
 
--- View company salary average
-SELECT COMPANY.cid, COMPANY.name, COMPANY_SALARY.avg_salary
+SELECT
+    COMPANY.cid,
+    COMPANY.name,
+    avg_salary,
+    avg_rating
 FROM COMPANY
 LEFT OUTER JOIN (
     SELECT cid, AVG(salary) as avg_salary
@@ -12,11 +13,6 @@ LEFT OUTER JOIN (
     GROUP BY cid
 ) AS COMPANY_SALARY
 ON COMPANY.cid = COMPANY_SALARY.cid
-ORDER BY COMPANY_SALARY.avg_salary DESC;
-
--- View company review average
-SELECT COMPANY.cid, COMPANY.name, COMPANY_REVIEW.avg_rating
-FROM COMPANY
 LEFT OUTER JOIN(
     SELECT cid, AVG(rating) as avg_rating
     FROM REVIEW
@@ -24,16 +20,9 @@ LEFT OUTER JOIN(
     GROUP BY cid
 ) AS COMPANY_REVIEW
 ON COMPANY.cid = COMPANY_REVIEW.cid
-ORDER BY COMPANY_REVIEW.avg_rating DESC;
 
-SELECT '----------------------' AS '';
-SELECT 'COMPANY PAGE FOR EACH COMPANY' AS '';
-SELECT '----------------------' AS '';
 
-SELECT '-------Query 2--------' AS '';
-SELECT 'LIST ALL JOBS POSTED BY THE COMPANY AND THEIR RELATED INFO' AS '';
-SELECT 'INCLUDING average salary and rating' AS '';
-SELECT '----------------------' AS '';
+SELECT 'Query 2: list all jobs posted by a company' AS '';
 
 SELECT JOB.jid, title, avg_salary, avg_review
 FROM JOB
@@ -51,24 +40,23 @@ LEFT OUTER JOIN (SELECT jid, AVG(rating) as avg_review
 ON JOB.jid = JOB_RATING.jid
 WHERE cid = 567586557;
 
-SELECT '-----Query 3------' AS '';
-SELECT '-----TERMS HIRED BY COMPANY------' AS '';
--- VIEW TERMS HIRED BY COMPANY
-SELECT term_num, COUNT(term_num)
-FROM (SELECT *
-        FROM PLACEMENT
-        WHERE cid = 567586557
-    ) as PLACES
+
+SELECT 'Query 3: breakdown number of students hired by student term number' AS '';
+
+SELECT term_num, COUNT(term_num) as hires
+FROM PLACEMENT
+WHERE cid = 567586557
 GROUP BY term_num;
 
-SELECT '----------------------' AS '';
-SELECT 'JOB PAGE' AS '';
-SELECT '----------------------' AS '';
-SELECT '-----Query 4------' AS '';
-SELECT '-----Browse all jobs------' AS '';
--- View all jobs, and their company, average salary and rating.
 
-SELECT JOB.jid, COMPANY.name as company_name, JOB.title as job_title, avg_salary, avg_rating
+SELECT 'Query 4: Browse Jobs page' AS '';
+
+SELECT
+    JOB.jid,
+    COMPANY.name as company_name,
+    JOB.title as job_title,
+    avg_salary,
+    avg_rating
 FROM JOB
 LEFT OUTER JOIN COMPANY
 ON JOB.cid = COMPANY.cid
@@ -86,9 +74,8 @@ LEFT OUTER JOIN (SELECT jid, AVG(rating) as avg_rating
 ON JOB.jid = JOB_RATING.jid
 ORDER BY JOB_SALARY.avg_salary DESC;
 
-SELECT '----------------------' AS '';
-SELECT '-----Query 5------' AS '';
-SELECT '-----Search job by tag------' AS '';
+
+SELECT 'Query 5: search job by tag' AS '';
 
 SELECT JOB.jid, JOB.title
 FROM JOB_TAG
@@ -97,9 +84,3 @@ ON JOB_TAG.tid = TAG.tid
 JOIN JOB
 ON JOB.jid = JOB_TAG.jid
 WHERE TAG.label = 'full-stack';
-
--- Note that the rating calculation is a little bit more complicated
--- than a simple SQL query so for space purposes we will not be including
--- it here. In general however, we will be treating each latter coop job
--- as winning the "battle" against a earlier coop job, and awarding mmr to
--- the latter job. See the link below for a more detailed description
