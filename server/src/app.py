72a2ccd5f2a034130a4a61ae81dddbf06c69d369
Justin Xu <xu.justin.j@gmail.com>
2021-07-03 00:07:16 -0400

    Add filtering by tag to the jobs endpoint

--- server/src/app.py
+++ server/src/app.py
@@ -1,23 +1,35 @@
 import json
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 import mysql.connector
 
 from utils.mysql import open_connection, get_cursor
 from utils.serialization import serialize
-from queries import AGGREGATE_JOBS
+from queries import *
 
 
-def list_jobs(cursor):
+def aggregate_jobs(cursor):
     cursor.execute(AGGREGATE_JOBS)
     for job in cursor:
+        print(job)
         yield {
             'jid': serialize(job.jid, as_type=int),
             'company_name': job.company_name,
             'job_title': job.job_title,
             'avg_salary': serialize(job.avg_salary, as_type=float, nullable=True),
             'avg_rating': serialize(job.avg_rating, as_type=float, nullable=True),
+        }
+
+
+def jobs_by_tag(cursor, tag):
+    print(tag)
+    cursor.execute(JOBS_BY_TAG, (tag,))
+    for job in cursor:
+        print(job)
+        yield {
+            'jid': serialize(job.jid, as_type=int),
+            'title': job.title,
         }
 
 
@@ -29,12 +41,15 @@ connection = open_connection()
 
 
 @app.route('/jobs', methods=['GET'])
-def home():
+def jobs():
     with get_cursor(connection) as cursor:
-        jobs_list = list(list_jobs(cursor))
-    return json.dumps(jobs_list)
+        if tag := request.args.get('tag', None):
+            jobs = list(jobs_by_tag(cursor, tag))
+        else:
+            jobs = list(aggregate_jobs(cursor))
+        return json.dumps(jobs)
 
 
 if __name__ == '__main__':
     app.run()
-    cnx.close()
+    connection.close()
--- server/src/queries.py
+++ server/src/queries.py
@@ -1,24 +1,82 @@
+AGGREGATE_COMPANIES = """
+SELECT
+    COMPANY.cid,
+    COMPANY.name,
+    avg_salary,
+    avg_rating
+FROM COMPANY
+LEFT OUTER JOIN (
+    SELECT cid, AVG(salary) as avg_salary
+    FROM PLACEMENT
+    WHERE salary IS NOT NULL
+    GROUP BY cid
+) AS COMPANY_SALARY
+ON COMPANY.cid = COMPANY_SALARY.cid
+LEFT OUTER JOIN(
+    SELECT cid, AVG(rating) as avg_rating
+    FROM REVIEW
+    WHERE rating IS NOT NULL
+    GROUP BY cid
+) AS COMPANY_REVIEW
+ON COMPANY.cid = COMPANY_REVIEW.cid;
+"""
+
+JOBS_BY_COMPANY = """
+SELECT JOB.jid, title, avg_salary, avg_review
+FROM JOB
+LEFT OUTER JOIN (SELECT jid, AVG(salary) as avg_salary
+                    FROM PLACEMENT
+                    WHERE salary IS NOT NULL
+                    GROUP BY jid
+                ) as JOB_SALARY
+ON JOB.jid = JOB_SALARY.jid
+LEFT OUTER JOIN (SELECT jid, AVG(rating) as avg_review
+                    FROM REVIEW
+                    WHERE rating IS NOT NULL
+                    GROUP BY jid
+                ) as JOB_RATING
+ON JOB.jid = JOB_RATING.jid
+WHERE cid = %s;
+"""
+
+STUDENTS_HIRED_BY_TERM = """
+SELECT term_num, COUNT(term_num) as hires
+FROM PLACEMENT
+WHERE cid = %s
+GROUP BY term_num;
+"""
+
 AGGREGATE_JOBS = """
 SELECT
-    JOB.jid as jid,
+    JOB.jid,
     COMPANY.name as company_name,
     JOB.title as job_title,
     avg_salary,
     avg_rating
 FROM JOB
 LEFT OUTER JOIN COMPANY
-    ON JOB.cid = COMPANY.cid
+ON JOB.cid = COMPANY.cid
 LEFT OUTER JOIN (SELECT jid, AVG(salary) as avg_salary
                     FROM PLACEMENT
                     WHERE salary IS NOT NULL
                     GROUP BY jid
                 ) as JOB_SALARY
-    ON JOB.jid = JOB_SALARY.jid
+ON JOB.jid = JOB_SALARY.jid
 LEFT OUTER JOIN (SELECT jid, AVG(rating) as avg_rating
                     FROM REVIEW
                     WHERE rating IS NOT NULL
                     GROUP BY jid
                 ) as JOB_RATING
-    ON JOB.jid = JOB_RATING.jid
+ON JOB.jid = JOB_RATING.jid
 ORDER BY JOB_SALARY.avg_salary DESC;
+"""
+
+JOBS_BY_TAG = """
+SELECT JOB.jid, JOB.title
+FROM JOB_TAG
+JOIN TAG
+ON JOB_TAG.tid = TAG.tid
+JOIN JOB
+ON JOB.jid = JOB_TAG.jid
+WHERE TAG.label = %s;
 """
